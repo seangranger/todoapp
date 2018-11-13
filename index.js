089f9e19bf349e947c467b9e4cb169781b46afed
SmallFry <nowatewat@gmail.com>
2018-11-12 18:15:34 -0700

    added item deletion. Made server solely responsible for list-going to undo in next commit

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 var subut = document.querySelector("[type = 'submit']");
 var inbox = document.querySelector("[type = 'text']");
-var addtodo = function(mssg){
+var ul = document.querySelector('ul')
+var masterlist = {};
+var addtodo = function(mssg,id){
   var todoitem = document.createElement("li");
   todoitem.innerText = mssg;
-  document.querySelector('ul').appendChild(todoitem);
+  var rembut = document.createElement('button');
+  rembut.innerText = 'Delete Item';
+  //add class to left align buttons?
+  rembut.addEventListener('click', function(){
+    var obj2del = {};
+    obj2del[id] = mssg;
+    var json = JSON.stringify(obj2del);
+    //this will jsut go away and we can use addtodo
+    //ul.removeChild(pardiv);
+    var xhr = new XMLHttpRequest();
+    xhr.open('DELETE','/todos');
+    //need to figure out what event below is
+    xhr.addEventListener('load',function(){
+      var respjson = JSON.parse(this.responseText);
+      ul.innerText = '';
+      for (var incitem in respjson){
+        addtodo(respjson[incitem],incitem);
+      };
+      console.log(respjson);
+    });
+    xhr.send(json);
+  });
+  todoitem.appendChild(rembut);
+  ul.appendChild(todoitem);
 };
-
+//everything should be coming from server
 var inittodos = function(){
   var xhr = new XMLHttpRequest();
 
@@ -14,7 +39,11 @@ var inittodos = function(){
   xhr.addEventListener('load',function(){
     var json = this.responseText;
     var petd = JSON.parse(json);
-    petd.forEach(addtodo);
+    for (var incitem in petd){
+      console.log(incitem);
+      //addtodo(petd[incitem]);
+      addtodo(petd[incitem],incitem);
+    };
   });
 
   xhr.send();
@@ -30,7 +59,13 @@ subut.addEventListener('click',function(){
     if(this.status !== 200){
       alert('Something went wrong.');
     }else{
-      addtodo(inbox.value);
+      //this shouldnt be written twice
+      var json = this.responseText;
+      var petd = JSON.parse(json);
+      ul.innerText = '';
+      for (var incitem in petd){
+        addtodo(petd[incitem],incitem);
+      };
       inbox.value = '';
     }
   });
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var http = require('http');
 var server = http.createServer();
 var fs = require('fs');
 var pages = ['/index.html','/index.js'];
-var dummydata = ['eat','pray','kill for satan'];
+var counter = 4;
+var dummydata = {1:'eat',2:'pray',3:'kill for satan'};
 server.on('listening',function(){
   console.log('listening...');
 });
@@ -15,7 +16,7 @@ server.on('request',function(req,res){
       res.write(data);
       res.end();
     });
-    //should i have nested if with get and post options within elsif below?
+    //should i have nested if with get and post options within else if below?
   }else if (reqpath === '/todos' && req.method === 'GET'){
     var json = JSON.stringify(dummydata);
     res.write(json);
@@ -27,11 +28,34 @@ server.on('request',function(req,res){
       body += data;
     });
     req.on('end', function(){
-      dummydata.push(body);
-      console.log(dummydata);
+      //this needs to distinguish between removals and additions
+      dummydata[counter] = JSON.parse(body);
+      counter += 1;
+      var json = JSON.stringify(dummydata);
+      res.statusCode = 200;
+      res.write(json);
+      res.end();
     });
-    res.statusCode = 200;
-    res.end();
+    //below can be folded into above else if?-think that would be good
+  }else if (reqpath === '/todos' && req.method === 'DELETE'){
+    var body = '';
+    req.on('data',function(data){
+      body += data;
+    });
+    req.on('end', function(){
+      console.log(body);
+      var json = JSON.parse(body);
+      for(var key in json){
+        console.log('this is dummydata[key] '+dummydata[key]);
+        delete dummydata[key];
+      }
+      var outjson = JSON.stringify(dummydata);
+      res.statusCode = 200;
+      console.log(outjson);
+      res.write(outjson);
+      res.end();
+      console.log('this is dummydata'+JSON.stringify(dummydata));
+    });
   }else{
     res.statusCode = 404;
     res.end();
